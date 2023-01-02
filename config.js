5230cbf836692fd4700e5800a18c556c8a227f82
Matias <mrodasr@outlook.es>
2023-01-02 03:25:24 -0300

    deploy config

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
     prod: {
         NODE_ENV: 'production',
         PORT: '3000',
-        IP: '127.0.0.1',
+        IP: '192.168.0.106',
         MONGODB_URI: 'mongodb://127.0.0.1:27017',
         DB_NAME: 'camping',
         HMAC_KEY: require('crypto').randomBytes(16).toString('hex')
